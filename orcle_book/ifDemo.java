9b2b2e3ff961ca4ea697d8fbf85baa9a4e008aa0
lisa leniashina <lleniashina@hq.bill.com>
2018-11-21 15:27:23 -0800

    java basics

---
+++ orcle_book/ifDemo.java
@@ -0,0 +1,42 @@
+/* Demonstrates the if */
+
+class ifDemo {
+
+	public static void main (String args[]){
+		int a, b, c;
+		a = 2;
+		b = 3;
+
+		if (a < b) {
+			System.out.println("a less than b");
+		}
+		if (a == b){
+			System.out.println(" you won't see it");
+		}
+
+		System.out.println();
+
+		c = a - b;
+
+		System.out.println(" c contains -1");
+		if (c >= 0){
+			System.out.println(" c is non-negative");
+		}
+		if (c < 0){
+			System.out.println(" c is negative");
+		}
+
+		System.out.println();
+
+		c = b - a;
+
+		System.out.println(" c contains 1");
+		if (c >= 0){
+			System.out.println(" c is non-negative");
+		}
+		if (c < 0){
+			System.out.println(" c is negative");
+		}
+
+	}
+}
